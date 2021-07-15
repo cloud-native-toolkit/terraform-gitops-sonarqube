e0a83498c2f3974aefc26cbeae85cbf1c81c8587
Sean Sundberg <seansund@us.ibm.com>
2021-07-15 17:41:46 -0500

    Fixes typo
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-sonarqube.tf
+++ test/stages/stage2-sonarqube.tf
@@ -7,5 +7,5 @@ module "gitops_sonarqube" {
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
   cluster_type             = module.dev_cluster.platform.type_code
   tls_secret_name          = module.dev_cluster.platform.tls_secret
-  kubeseal_cert            = module.argocd-bootstrap.sealed_secret_cert
+  kubeseal_cert            = module.argocd-bootstrap.sealed_secrets_cert
 }
