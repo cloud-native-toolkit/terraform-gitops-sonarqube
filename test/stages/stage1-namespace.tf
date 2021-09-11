8c674e175f22ae20b94f860b86c08de2047c234e
Sean Sundberg <seansund@us.ibm.com>
2021-09-11 00:45:02 -0500

    Updates metadata
    - Makes cluster dependency optional (only needed for IKS)
    - Updates kubeseal_cert dependency satisfied by gitops repo instead of argocd bootstrap
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -4,8 +4,6 @@ module "gitops_namespace" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   name = var.namespace
-  argocd_namespace       = module.argocd-bootstrap.argocd_namespace
-  argocd_service_account = module.argocd-bootstrap.argocd_service_account
   server_name              = module.gitops.server_name
 }
 
