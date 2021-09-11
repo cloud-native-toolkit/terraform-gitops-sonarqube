b38ecccd5b1ecb7a3c803e9a2c6e10c058768342
Sean Sundberg <seansund@us.ibm.com>
2021-09-11 01:26:20 -0500

    Updates metadata (#6)
    - Makes cluster dependency optional (only needed for IKS)
    - Updates kubeseal_cert dependency satisfied by gitops repo instead of argocd bootstrap
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-sonarqube.tf
+++ test/stages/stage2-sonarqube.tf
@@ -4,9 +4,6 @@ module "gitops_sonarqube" {
   gitops_config            = module.gitops.gitops_config
   git_credentials          = module.gitops.git_credentials
   namespace                = module.gitops_namespace.name
-  cluster_ingress_hostname = module.dev_cluster.platform.ingress
-  cluster_type             = module.dev_cluster.platform.type_code
-  tls_secret_name          = module.dev_cluster.platform.tls_secret
-  kubeseal_cert            = module.argocd-bootstrap.sealed_secrets_cert
+  kubeseal_cert            = module.gitops.sealed_secrets_cert
   server_name              = module.gitops.server_name
 }
