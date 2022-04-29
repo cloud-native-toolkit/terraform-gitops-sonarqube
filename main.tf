b9bcca42faedb656b7e676ea04cde84db20704d8
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-04-29 21:01:11 +0530

    Adding scc for namespace
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- main.tf
+++ main.tf
@@ -180,6 +180,21 @@ module "service_account" {
   server_name = var.server_name
 }
 
+module setup_group_scc {
+  depends_on = [module.service_account]
+
+  source = "github.com/cloud-native-toolkit/terraform-gitops-sccs.git"
+
+  gitops_config = var.gitops_config
+  git_credentials = var.git_credentials
+  namespace = var.namespace
+  service_account = ""
+  sccs = ["anyuid"]
+  server_name = var.server_name
+  group = true
+}
+
+
 resource null_resource setup_gitops {
   depends_on = [null_resource.setup_chart, module.service_account]
 
