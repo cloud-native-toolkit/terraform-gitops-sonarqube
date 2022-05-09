64ecf83ca275023bf959c65fc8d51b0aedc2bbd4
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-05-09 21:39:50 +0530

    Added cluster version
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- test/stages/stage1-software-olm.tf
+++ test/stages/stage1-software-olm.tf
@@ -2,6 +2,6 @@ module "dev_software_olm" {
   source = "github.com/cloud-native-toolkit/terraform-k8s-olm"
 
   cluster_config_file      = module.dev_cluster.config_file_path
-  cluster_version          = module.dev_cluster.version
+  cluster_version          = var.cluster_version
   cluster_type             = var.cluster_type
 }
