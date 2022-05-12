a4688f68ccc0bcfdca7aa112f40ae64807feff47
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-05-12 23:52:40 +0530

    Added SA module back in main tf
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- main.tf
+++ main.tf
@@ -200,7 +200,7 @@ module seal_secrets {
   label         = local.name
 }
 
-/*module "service_account" {
+module "service_account" {
   source = "github.com/cloud-native-toolkit/terraform-gitops-service-account.git"
 
   gitops_config = var.gitops_config
@@ -234,10 +234,10 @@ module seal_secrets {
   ]
   #rbac_cluster_scope = true
 
-}*/
+}
 
 module setup_group_scc {
-  #depends_on = [module.service_account]
+  depends_on = [module.service_account]
 
   source = "github.com/cloud-native-toolkit/terraform-gitops-sccs.git"
 
@@ -252,7 +252,7 @@ module setup_group_scc {
 
 
 resource null_resource setup_gitops {
-  depends_on = [null_resource.create_yaml, module.seal_secrets]
+  depends_on = [null_resource.create_yaml,module.service_account, module.seal_secrets]
 
   triggers = {
     name = local.name
