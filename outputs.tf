20223420c876d7f323ab45a980640b34c4ea7c0a
Sean Sundberg <seansund@us.ibm.com>
2022-09-05 00:50:38 -0500

    Changes to use gitops provider
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,41 +2,41 @@
 output "name" {
   description = "The name of the module"
   value       = local.name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
   value       = var.namespace
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
   value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
-  value       = "base"
-  depends_on  = [null_resource.setup_gitops]
+  value       = local.type
+  depends_on  = [gitops_module.module]
 }
 
 output "postgresql" {
   description = "Properties for an existing postgresql database"
   value       = local.postgresql
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.module]
 }
